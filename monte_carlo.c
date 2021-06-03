d5cb2bd712b235c6f2e95ef3beeaa40ec2547641
chanverse <68290563+chanverse@users.noreply.github.com>
2021-06-03 22:46:58 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -40,19 +40,19 @@ int main(void) {
 }
 float mc_pi(int n){
     int c_in=0;
-    float a,b,c;
+    float a,b,c,d,e;
     for(int j=1;j<=n;j++){
          a=frandom();
          b=frandom();
          c=sqrt((a*a)+(b*b));
         if(c<=1){
-            c_in++;
-
+            c_in++;}
+      else{
+        continue;
         }
-
-    }
-    float d=(c_in/n);
-    float e=(4*d);
+     
+    d=(c_in/n);
+    e=(4*d);
     return (e);
 }
 
